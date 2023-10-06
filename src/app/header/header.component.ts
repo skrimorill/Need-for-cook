2bcfcc1e9ac35395c8884a9776a7adcf963d81c3
Paul <skrimorill@mail.ru>
2023-10-06 09:29:34 +0300

    added dropdown

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isMenuOpened: boolean = false;
+  @Output() featureSelected = new EventEmitter<string>()
 
   toggleMenu(): void {
     this.isMenuOpened = !this.isMenuOpened;
+  }
+
+  onSelect(feature: string): void { 
+    this.featureSelected.emit(feature);
   }
 
   constructor() { }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Some name', 'some description', 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Frecipes%2F2023%2F2023-06-zucchini-pancakes%2Fzucchini-pancakes-1'),
-    new Recipe('Some name', 'some description', 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Frecipes%2F2023%2F2023-06-zucchini-pancakes%2Fzucchini-pancakes-1')
+    new Recipe('Another name', 'some description', 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Frecipes%2F2023%2F2023-06-zucchini-pancakes%2Fzucchini-pancakes-1')
   ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
